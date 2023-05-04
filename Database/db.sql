2bc0c4e2cbae26d264591c606d4ec4991a8a5187
xNadhmi <80103454+xNadhmi@users.noreply.github.com>
2023-05-04 11:27:55 +0200

    Update db.sql

--- Database/db.sql
+++ Database/db.sql
@@ -1,4 +1,9 @@
 
+
+CREATE DATABASE 'monopoly' IF NOT EXISTS CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+
+
 -- TABLE STRUCTURE FOR 'players' TABLE
 
 CREATE TABLE IF NOT EXISTS players (
@@ -18,11 +23,25 @@ INSERT INTO players (id, name, avatar, money, status) VALUES (1000, "default", "
 CREATE TABLE IF NOT EXISTS tiles (
 	id INT AUTO_INCREMENT PRIMARY KEY,											-- Global tile id, also indicates position
 	name VARCHAR(50) NOT NULL,													-- Holds the name of the property on the tile
-	description VARCHAR(100),													-- Holds description of the tile, if any
+	description VARCHAR(250),													-- Holds description of the tile, if any
 	price NOT NULL,																-- Holds the cost of the property on the tile
-	rent_percentage FLOAT 15,													-- Holds the percentage of original price a landing player has to pay
-	bank_sale FLOAT 50,															-- Holds the percentage of original price a player can sell property to the bank
+	rent_percentage FLOAT DEFAULT 15,											-- Holds the percentage of original price a landing player has to pay
+	bank_sale FLOAT DEFAULT 50,													-- Holds the percentage of original price a player can sell property to the bank
 	type ENUM("go", "jail", "go_jail", "street", "utility", "chance", "community", "tax") NOT NULL,	-- Holds tile type
-	owner INT 1000,																-- Holds the id of the player who owns the tile's property
+	owner INT DEFAULT 1000,														-- Holds the id of the player who owns the tile's property
 	FOREIGN KEY (owner) REFERENCES players(id),									-- Key reference to the "players" table
+) ENGINE=INNODB;
+
+
+-- TABLE STRUCTURE FOR 'cards' TABLE
+
+CREATE TABLE IF NOT EXISTS cards (
+	id INT AUTO_INCREMENT PRIMARY KEY,											-- Global card id
+	type ENUM("chance", "community") NOT NULL,									-- Holds the type of the
+	name VARCHAR(50) NOT NULL,													-- Holds the name of the card
+	description VARCHAR(250),													-- Holds description of the card, if any
+	action VARCHAR(250) NOT NULL,												-- Holds the action text of the card
+	effect ENUM("money", "position") NOT NULL,									-- Holds the type of the effect of the card
+	value INT DEFAULT 0,														-- Holds the value of the effect, positive or negative
+	target ("current_player", "other_players", "everyone"),						-- Holds the card's affected target
 ) ENGINE=INNODB;
